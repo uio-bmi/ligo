8758b2a7c0d7cca20752934820f53ab3b343c104
pavlovicmilena <milenapavlovic775@gmail.com>
2023-11-03 10:29:02 +0100

    fix bionumpy dependency

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
     author="Milena Pavlovic",
     author_email="milenpa@student.matnat.uio.no",
     url="https://github.com/uio-bmi/ligo",
-    install_requires=["pandas", "bionumpy", "numpy", "olga", "pyyaml", "airr<1.4", "plotly", "pystache", "scipy", 'npstructures', 'stitchr',
+    install_requires=["pandas", "bionumpy==0.2.26", "numpy", "olga", "pyyaml", "airr<1.4", "plotly", "pystache", "scipy", 'npstructures', 'stitchr',
                       'IMGTgeneDL', 'dill'],
     classifiers=[
         "Programming Language :: Python :: 3",
